360de9e606092cc6dc2bd69fc6b0e643bbc4672a
jenhk03 <jendrajahk0303@gmail.com>
2023-07-05 17:42:47 +0700

    Create connection1.php

---
+++ connection1.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = "localhost";
+$db_name = "id19580627_dbcar";
+$db_user = "id19580627_dbcar";
+$db_pwd = "Jhk1234*";
+$connect = mysqli_connect($db_host, $db_user, $db_pwd, $db_name) or die("MySQL tidak terhubung!");
+?>
